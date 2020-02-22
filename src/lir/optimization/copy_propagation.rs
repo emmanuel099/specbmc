3b719f2faea53c8075739830007ac74463115786
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2020-02-22 13:34:17 +0100

    LIR: Add Copy Propagation

---
+++ src/lir/optimization/copy_propagation.rs
@@ -0,0 +1,87 @@
+use crate::error::Result;
+use crate::expr;
+use crate::lir;
+use std::collections::HashMap;
+
+pub fn propagate_copies(program: &mut lir::Program) -> Result<()> {
+    let copies = determine_copied_variables(program.nodes());
+    if copies.is_empty() {
+        return Ok(());
+    }
+
+    replace_copied_variables(&mut program.nodes_mut(), &copies);
+
+    Ok(())
+}
+
+fn determine_copied_variables(nodes: &[lir::Node]) -> HashMap<expr::Variable, expr::Variable> {
+    let mut copies = HashMap::new();
+
+    nodes.iter().for_each(|node| match node {
+        lir::Node::Let { var, expr } => match expr.operator() {
+            expr::Operator::Variable(src_var) => {
+                copies.insert(var.clone(), src_var.clone());
+            }
+            _ => (),
+        },
+        _ => (),
+    });
+
+    resolve_copies_of_copies(&mut copies);
+
+    copies
+}
+
+fn replace_copied_variables(
+    nodes: &mut Vec<lir::Node>,
+    copies: &HashMap<expr::Variable, expr::Variable>,
+) {
+    let replace_if_copied = |var: &mut expr::Variable| match copies.get(var) {
+        Some(src_var) => *var = src_var.clone(),
+        None => (),
+    };
+
+    for node in nodes {
+        match node {
+            lir::Node::Let { expr, .. } => {
+                expr.variables_mut().into_iter().for_each(replace_if_copied)
+            }
+            lir::Node::Assert { cond } | lir::Node::Assume { cond } => {
+                cond.variables_mut().into_iter().for_each(replace_if_copied)
+            }
+            _ => (),
+        }
+    }
+}
+
+/// Resolves copies of copies to avoid that replace_copied_variables needs to be called multiple times.
+///
+/// Given:
+/// b = a
+/// c = b
+///
+/// Resolved:
+/// b = a
+/// c = a
+fn resolve_copies_of_copies(copies: &mut HashMap<expr::Variable, expr::Variable>) {
+    loop {
+        let mut prop: Option<(expr::Variable, expr::Variable)> = None;
+        for (copy, var) in copies.iter() {
+            match copies.get(var) {
+                Some(src_var) => {
+                    prop = Some((copy.clone(), src_var.clone()));
+                    break;
+                }
+                None => (),
+            }
+        }
+
+        match prop {
+            Some((copy, src_var)) => {
+                copies.insert(copy, src_var);
+                continue;
+            }
+            None => break,
+        }
+    }
+}
--- src/lir/optimization/mod.rs
+++ src/lir/optimization/mod.rs
@@ -1,9 +1,11 @@
 use crate::error::Result;
 use crate::lir;
 
+mod copy_propagation;
 mod dead_code_elimination;
 
 pub fn optimize(program: &mut lir::Program) -> Result<()> {
+    copy_propagation::propagate_copies(program)?;
     dead_code_elimination::eliminate_dead_code(program)?;
 
     Ok(())
