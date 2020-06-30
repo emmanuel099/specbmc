41835fd119e8cc792da3d03029ce409a0a8ecdeb
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2020-06-30 13:10:02 +0200

    hir: Simplify explicit effects and non-spec obs-equiv and make them more generic

--- src/hir/transformation/explicit_effects.rs
+++ src/hir/transformation/explicit_effects.rs
@@ -1,5 +1,5 @@
 use crate::error::*;
-use crate::expr;
+use crate::expr::{BranchTargetBuffer, Cache, Expression, PatternHistoryTable};
 use crate::hir::{Effect, Instruction, Operation, Program};
 use crate::ir::Transform;
 
@@ -16,15 +16,9 @@ impl Transform<Program> for ExplicitEffects {
     }
 
     fn transform(&self, program: &mut Program) -> Result<()> {
-        let encoder = InstructionEffectEncoder::new(
-            expr::Cache::variable(),
-            expr::BranchTargetBuffer::variable(),
-            expr::PatternHistoryTable::variable(),
-        );
-
         for block in program.control_flow_graph_mut().blocks_mut() {
             for instruction in block.instructions_mut() {
-                encoder.encode_effects(instruction)?;
+                encode_effects(instruction)?;
             }
         }
 
@@ -32,91 +26,61 @@ impl Transform<Program> for ExplicitEffects {
     }
 }
 
-pub(super) struct InstructionEffectEncoder {
-    cache: expr::Variable,
-    btb: expr::Variable,
-    pht: expr::Variable,
+fn encode_effects(instruction: &mut Instruction) -> Result<()> {
+    if !instruction.has_effects() {
+        return Ok(());
+    }
+
+    let effect_operations = instruction
+        .effects()
+        .iter()
+        .map(encode_effect)
+        .collect::<Result<Vec<Operation>>>()?;
+
+    instruction.add_operations(&effect_operations);
+
+    Ok(())
 }
 
-impl InstructionEffectEncoder {
-    pub fn new(cache: expr::Variable, btb: expr::Variable, pht: expr::Variable) -> Self {
-        Self { cache, btb, pht }
-    }
-
-    pub fn encode_effects(&self, instruction: &mut Instruction) -> Result<()> {
-        if !instruction.has_effects() {
-            return Ok(());
+fn encode_effect(effect: &Effect) -> Result<Operation> {
+    match effect {
+        Effect::Conditional { condition, effect } => {
+            if let Operation::Assign { variable, expr } = encode_effect(effect)? {
+                Operation::assign(
+                    variable.clone(),
+                    Expression::ite(condition.clone(), expr, variable.into())?,
+                )
+            } else {
+                unimplemented!()
+            }
         }
-
-        let effect_operations = instruction
-            .effects()
-            .iter()
-            .map(|effect| self.encode_effect(effect))
-            .collect::<Result<Vec<Operation>>>()?;
-
-        instruction.add_operations(&effect_operations);
-
-        Ok(())
+        Effect::CacheFetch { address, bit_width } => encode_cache_fetch_effect(address, *bit_width),
+        Effect::BranchTarget { location, target } => encode_branch_target_effect(location, target),
+        Effect::BranchCondition {
+            location,
+            condition,
+        } => encode_branch_condition_effect(location, condition),
     }
+}
 
-    fn encode_effect(&self, effect: &Effect) -> Result<Operation> {
-        match effect {
-            Effect::Conditional { condition, effect } => {
-                if let Operation::Assign { variable, expr } = self.encode_effect(effect)? {
-                    Operation::assign(
-                        variable.clone(),
-                        expr::Expression::ite(condition.clone(), expr, variable.into())?,
-                    )
-                } else {
-                    unimplemented!()
-                }
-            }
-            Effect::CacheFetch { address, bit_width } => {
-                self.encode_cache_fetch_effect(address, *bit_width)
-            }
-            Effect::BranchTarget { location, target } => {
-                self.encode_branch_target_effect(location, target)
-            }
-            Effect::BranchCondition {
-                location,
-                condition,
-            } => self.encode_branch_condition_effect(location, condition),
-        }
-    }
+fn encode_cache_fetch_effect(address: &Expression, bit_width: usize) -> Result<Operation> {
+    let cache = Cache::variable();
+    let fetch = Cache::fetch(bit_width, cache.clone().into(), address.clone())?;
+    Operation::assign(cache, fetch)
+}
 
-    fn encode_cache_fetch_effect(
-        &self,
-        address: &expr::Expression,
-        bit_width: usize,
-    ) -> Result<Operation> {
-        let fetch = expr::Cache::fetch(bit_width, self.cache.clone().into(), address.clone())?;
-        Operation::assign(self.cache.clone(), fetch)
-    }
+fn encode_branch_target_effect(location: &Expression, target: &Expression) -> Result<Operation> {
+    let btb = BranchTargetBuffer::variable();
+    let track = BranchTargetBuffer::track(btb.clone().into(), location.clone(), target.clone())?;
+    Operation::assign(btb, track)
+}
 
-    fn encode_branch_target_effect(
-        &self,
-        location: &expr::Expression,
-        target: &expr::Expression,
-    ) -> Result<Operation> {
-        let track = expr::BranchTargetBuffer::track(
-            self.btb.clone().into(),
-            location.clone(),
-            target.clone(),
-        )?;
-        Operation::assign(self.btb.clone(), track)
-    }
-
-    fn encode_branch_condition_effect(
-        &self,
-        location: &expr::Expression,
-        condition: &expr::Expression,
-    ) -> Result<Operation> {
-        let taken = expr::PatternHistoryTable::taken(self.pht.clone().into(), location.clone())?;
-        let not_taken =
-            expr::PatternHistoryTable::not_taken(self.pht.clone().into(), location.clone())?;
-        Operation::assign(
-            self.pht.clone(),
-            expr::Expression::ite(condition.clone(), taken, not_taken)?,
-        )
-    }
+fn encode_branch_condition_effect(
+    location: &Expression,
+    condition: &Expression,
+) -> Result<Operation> {
+    let pht = PatternHistoryTable::variable();
+    let taken = PatternHistoryTable::taken(pht.clone().into(), location.clone())?;
+    let not_taken = PatternHistoryTable::not_taken(pht.clone().into(), location.clone())?;
+    Operation::assign(pht, Expression::ite(condition.clone(), taken, not_taken)?)
 }
--- src/hir/transformation/non_spec_obs_equiv.rs
+++ src/hir/transformation/non_spec_obs_equiv.rs
@@ -1,7 +1,6 @@
 use crate::environment::Environment;
 use crate::error::Result;
 use crate::expr::{BranchTargetBuffer, Cache, Expression, PatternHistoryTable, Sort, Variable};
-use crate::hir::transformation::explicit_effects::InstructionEffectEncoder;
 use crate::hir::{Operation, Program};
 use crate::ir::Transform;
 
@@ -21,71 +20,144 @@ impl NonSpecObsEquivalence {
         }
     }
 
-    fn observable_variables_nonspec(&self) -> Vec<Expression> {
-        let mut variables = Vec::new();
-
+    fn add_initial_spec_nonspec_equivalence_constraints(
+        &self,
+        program: &mut Program,
+    ) -> Result<()> {
+        let mut eq_vars: Vec<(Variable, Variable)> = Vec::new();
         if self.cache_available {
-            variables.push(cache_nonspec().into());
+            let cache_spec = Cache::variable();
+            eq_vars.push((cache_nonspec(), cache_spec));
         }
         if self.btb_available {
-            variables.push(btb_nonspec().into());
+            let btb_spec = BranchTargetBuffer::variable();
+            eq_vars.push((btb_nonspec(), btb_spec));
         }
         if self.pht_available {
-            variables.push(pht_nonspec().into());
+            let pht_spec = PatternHistoryTable::variable();
+            eq_vars.push((pht_nonspec(), pht_spec));
         }
 
-        variables
-    }
-
-    fn encode_nonspec_equivalence(&self, program: &mut Program) -> Result<()> {
         let cfg = program.control_flow_graph_mut();
-
-        // Initially low-equivalent, therefore assign same content in entry
         let entry_block = cfg.entry_block_mut()?;
-        if self.cache_available {
+
+        for (lhs, rhs) in eq_vars {
             entry_block
-                .assign(cache_nonspec(), Cache::variable().into())?
+                .assume(Expression::equal(lhs.into(), rhs.into())?)?
                 .labels_mut()
                 .pseudo();
-        }
-        if self.btb_available {
-            entry_block
-                .assign(btb_nonspec(), BranchTargetBuffer::variable().into())?
-                .labels_mut()
-                .pseudo();
-        }
-        if self.pht_available {
-            entry_block
-                .assign(pht_nonspec(), PatternHistoryTable::variable().into())?
-                .labels_mut()
-                .pseudo();
-        }
-
-        let encoder = InstructionEffectEncoder::new(cache_nonspec(), btb_nonspec(), pht_nonspec());
-
-        for block in cfg
-            .blocks_mut()
-            .iter_mut()
-            .filter(|block| !block.is_transient())
-        {
-            for instruction in block.instructions_mut() {
-                // Collect nonspec instruction effects
-                encoder.encode_effects(instruction)?;
-
-                // Nonspec equivalent observations
-                if instruction
-                    .operations()
-                    .iter()
-                    .any(Operation::is_observable)
-                {
-                    instruction.add_operation(Operation::indistinguishable(
-                        self.observable_variables_nonspec(),
-                    ));
-                }
-            }
         }
 
         Ok(())
+    }
+}
+
+impl Transform<Program> for NonSpecObsEquivalence {
+    fn name(&self) -> &'static str {
+        "NonSpecObsEquivalence"
+    }
+
+    fn description(&self) -> &'static str {
+        "Add non-speculative observational equivalence constraints"
+    }
+
+    fn transform(&self, program: &mut Program) -> Result<()> {
+        add_nonspec_operations(program)?;
+        self.add_initial_spec_nonspec_equivalence_constraints(program)?;
+
+        Ok(())
+    }
+}
+
+fn add_nonspec_operations(program: &mut Program) -> Result<()> {
+    program
+        .control_flow_graph_mut()
+        .blocks_mut()
+        .iter_mut()
+        .filter(|block| !block.is_transient())
+        .for_each(|block| {
+            block.instructions_mut().iter_mut().for_each(|instruction| {
+                let nonspec_operations = instruction
+                    .operations()
+                    .iter()
+                    .filter_map(|op| {
+                        if operation_requires_nonspec_equivalent(op) {
+                            Some(create_nonspec_operation_equivalent(op))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<Operation>>();
+
+                instruction.add_operations(&nonspec_operations);
+            });
+        });
+
+    Ok(())
+}
+
+fn variable_requires_nonspec_equivalent(var: &Variable) -> bool {
+    match var.sort() {
+        Sort::Cache | Sort::BranchTargetBuffer | Sort::PatternHistoryTable => true,
+        _ => false,
+    }
+}
+
+fn operation_requires_nonspec_equivalent(operation: &Operation) -> bool {
+    match operation {
+        Operation::Assert { .. }
+        | Operation::Assume { .. }
+        | Operation::Observable { .. }
+        | Operation::Indistinguishable { .. } => {
+            let variables_read = operation.variables_read();
+            variables_read
+                .into_iter()
+                .any(variable_requires_nonspec_equivalent)
+        }
+        _ => {
+            let variables_written = operation.variables_written();
+            variables_written
+                .into_iter()
+                .any(variable_requires_nonspec_equivalent)
+        }
+    }
+}
+
+fn replace_variable_with_nonspec_equivalent(var: &mut Variable) {
+    match var.sort() {
+        Sort::Cache => *var = cache_nonspec(),
+        Sort::BranchTargetBuffer => *var = btb_nonspec(),
+        Sort::PatternHistoryTable => *var = pht_nonspec(),
+        _ => {}
+    };
+}
+
+fn create_nonspec_operation_equivalent(op: &Operation) -> Operation {
+    match op {
+        Operation::Observable { exprs } => {
+            // Observable is a special case as we want observable microarchitectual components to be
+            // indistinguishable without speculation. Therefore, we emit an indistinguishable operation instead.
+            let mut nonspec_exprs = exprs.to_owned();
+            nonspec_exprs.iter_mut().for_each(|expr| {
+                expr.variables_mut()
+                    .into_iter()
+                    .for_each(replace_variable_with_nonspec_equivalent);
+            });
+            Operation::indistinguishable(nonspec_exprs)
+        }
+        _ => {
+            // Simply clone the operation and replace e.g. cache variables with their non-speculative counterparts.
+            let mut nonspec_op = op.to_owned();
+            nonspec_op
+                .variables_read_mut()
+                .into_iter()
+                .for_each(replace_variable_with_nonspec_equivalent);
+            nonspec_op
+                .variables_written_mut()
+                .into_iter()
+                .for_each(replace_variable_with_nonspec_equivalent);
+            nonspec_op
+        }
     }
 }
 
@@ -99,20 +171,4 @@ fn btb_nonspec() -> Variable {
 
 fn pht_nonspec() -> Variable {
     Variable::new("_pht_ns", Sort::pattern_history_table())
-}
-
-impl Transform<Program> for NonSpecObsEquivalence {
-    fn name(&self) -> &'static str {
-        "NonSpecObsEquivalence"
-    }
-
-    fn description(&self) -> &'static str {
-        "Add non-speculative observational equivalence constraints"
-    }
-
-    fn transform(&self, program: &mut Program) -> Result<()> {
-        self.encode_nonspec_equivalence(program)?;
-
-        Ok(())
-    }
 }
