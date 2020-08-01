9d5bb46cd86082e3ad04d17c97566a88bc5b75f8
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2020-08-01 23:15:19 +0200

    ci: Run System Tests (#7)

---
+++ test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, PIPE, TimeoutExpired
+import shlex
+import yaml
+import time
+import os
+from pathlib import Path
+
+SOLVER = os.getenv('SOLVER', 'yices2')
+DEFAULT_ARGS = os.getenv('DEFAULT_ARGS', '--skip-cex')
+SPECBMC_BIN = os.getenv('SPECBMC_BIN', 'target/debug/specbmc')
+TIMEOUT = os.getenv('TIMEOUT', 60)  # seconds
+
+# specbmc exit codes
+EXIT_CODE_SAFE = 0
+EXIT_CODE_UNSAFE = 2
+
+
+def get_expected_value_from_environment(env_file: str):
+    with open(env_file, 'r') as f:
+        env = yaml.safe_load(f)
+        expected = env['test']['expect']
+        if not expected in ['safe', 'unsafe']:
+            raise Exception('expected value must be safe or unsafe')
+        return expected
+
+
+def run_test(test_file, env_file):
+    print(f'test {test_file} with environment {env_file} ... ',
+          end='', flush=True)
+
+    try:
+        expected = get_expected_value_from_environment(env_file)
+    except:
+        print('error, failed get expected value from environment')
+        return False
+
+    try:
+        args = [SPECBMC_BIN, test_file, '--env', env_file, '--solver', SOLVER]
+        args += shlex.split(DEFAULT_ARGS)
+        start_time = time.time()
+        proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        (output, err) = proc.communicate(timeout=TIMEOUT)
+        exit_code = proc.wait(timeout=TIMEOUT)
+        end_time = time.time()
+    except TimeoutExpired:
+        proc.kill()
+        print('timeout')
+        return False
+
+    if not exit_code in [EXIT_CODE_SAFE, EXIT_CODE_UNSAFE]:
+        print('error, output was:')
+        print(output.decode())
+        print(err.decode())
+        return False
+
+    actual = 'safe' if exit_code == EXIT_CODE_SAFE else 'unsafe'
+    elapsed = '{:0.2f}s'.format(end_time - start_time)
+
+    if expected != actual:
+        print(f'failed, expected {expected} but was {actual}, took {elapsed}')
+        return False
+
+    print(f'ok, took {elapsed}')
+    return True
+
+
+def find_tests():
+    return Path('test').rglob('*.muasm')
+
+
+def find_environments_for_test(test_file):
+    env_prefix = test_file.stem
+    return test_file.parent.glob(f'{env_prefix}.*yaml')
+
+
+print(f'SOLVER={SOLVER}')
+print(f'DEFAULT_ARGS={DEFAULT_ARGS}')
+print(f'SPECBMC_BIN={SPECBMC_BIN}')
+print(f'TIMEOUT={TIMEOUT}s')
+
+print()
+
+some_test_failed = False
+
+for test_file in find_tests():
+    for env_file in find_environments_for_test(test_file):
+        if not run_test(test_file, env_file):
+            some_test_failed = True
+
+print()
+
+if some_test_failed:
+    print("Some tests failed!")
+    exit(1)
+
+print("All tests passed.")
+exit(0)
