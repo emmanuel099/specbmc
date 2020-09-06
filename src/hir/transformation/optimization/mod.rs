22c6d5f50d2f44d306acccb7c456fdb9b194cbb3
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2020-09-06 16:40:27 +0200

    hir: Run RedundantInstructionElimination optimization only once
    RedundantInstructionElimination can become quite expensive for
    large programs (and large unwinding bounds).
    
    Maybe we can improve RedundantInstructionElimination by storing
    hashes instead of Instruction copies. Meaning that the available
    instruction analysis returns hashes + references, and then we
    can check for duplicates by comparing the hash and onyl if the
    hash matches we need to retrieve the actual instruction to
    compare them. This would require a lot less memory.

--- src/hir/transformation/optimization/mod.rs
+++ src/hir/transformation/optimization/mod.rs
@@ -30,7 +30,9 @@ pub trait Optimization {
 }
 
 pub struct Optimizer {
-    optimizations: Vec<Box<dyn Optimization>>,
+    pre_optimizations: Vec<Box<dyn Optimization>>,
+    repeated_optimizations: Vec<Box<dyn Optimization>>,
+    post_optimizations: Vec<Box<dyn Optimization>>,
     repetitions: usize,
 }
 
@@ -45,33 +47,41 @@ impl Optimizer {
 
     pub fn none() -> Self {
         Self {
-            optimizations: Vec::new(),
+            pre_optimizations: Vec::new(),
+            repeated_optimizations: Vec::new(),
+            post_optimizations: Vec::new(),
             repetitions: 0,
         }
     }
 
     pub fn basic() -> Self {
         Self {
-            optimizations: vec![
+            pre_optimizations: Vec::new(),
+            repeated_optimizations: vec![
+                Box::new(ConstantFolding::new()),
+                Box::new(ConstantPropagation::new()),
                 Box::new(CopyPropagation::new()),
+                Box::new(ExpressionSimplification::new()),
                 Box::new(PhiElimination::new()),
-                Box::new(RedundantInstructionElimination::new()),
+                Box::new(DeadCodeElimination::new()),
             ],
+            post_optimizations: Vec::new(),
             repetitions: 3,
         }
     }
 
     pub fn full() -> Self {
         Self {
-            optimizations: vec![
+            pre_optimizations: Vec::new(),
+            repeated_optimizations: vec![
                 Box::new(ConstantFolding::new()),
                 Box::new(ConstantPropagation::new()),
                 Box::new(CopyPropagation::new()),
                 Box::new(ExpressionSimplification::new()),
                 Box::new(PhiElimination::new()),
-                Box::new(RedundantInstructionElimination::new()),
                 Box::new(DeadCodeElimination::new()),
             ],
+            post_optimizations: vec![Box::new(RedundantInstructionElimination::new())],
             repetitions: 5,
         }
     }
@@ -87,10 +97,14 @@ impl Transform<ControlFlowGraph> for Optimizer {
     }
 
     fn transform(&self, cfg: &mut ControlFlowGraph) -> Result<()> {
+        for optimization in &self.pre_optimizations {
+            optimization.optimize(cfg)?;
+        }
+
         for _ in 1..=self.repetitions {
             let mut unchanged = true;
 
-            for optimization in &self.optimizations {
+            for optimization in &self.repeated_optimizations {
                 let result = optimization.optimize(cfg)?;
                 if result == OptimizationResult::Changed {
                     unchanged = false;
@@ -100,6 +114,10 @@ impl Transform<ControlFlowGraph> for Optimizer {
             if unchanged {
                 break;
             }
+        }
+
+        for optimization in &self.post_optimizations {
+            optimization.optimize(cfg)?;
         }
 
         Ok(())
